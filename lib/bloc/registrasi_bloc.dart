e4544eed7efb111bf4ff17bbcf997219cb2d7d77
rizkizaky14 <rizki.zaky10@gmail.com>
2024-06-15 17:14:17 +0700

    add reg bloc

---
+++ lib/bloc/registrasi_bloc.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+import 'package:toko_kita/helpers/api.dart';
+import 'package:toko_kita/helpers/api_url.dart';
+import 'package:toko_kita/model/registrasi.dart';
+
+class RegistasiBloc {
+  static Future<Registrasi> registrasi(
+      {String? name, String? email, String? password}) async {
+    String apiUrl = ApiUrl.registrasi;
+
+    var body = {"name": name, "email": email, "password": password};
+    var response = await Api().post(apiUrl, body);
+    var jsonObj = json.decode(response.body);
+    return Registrasi.fromJson(jsonObj);
+  }
+}
